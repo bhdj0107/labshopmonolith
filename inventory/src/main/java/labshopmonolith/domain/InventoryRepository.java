b6968735ad31debbf399234236a7bb74504d89c7
bhdj0107 <43875772+bhdj0107@users.noreply.github.com>
2025-02-24 09:06:09 +0900

    commit

---
+++ inventory/src/main/java/labshopmonolith/domain/InventoryRepository.java
@@ -0,0 +1,12 @@
+package labshopmonolith.domain;
+
+import labshopmonolith.domain.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//<<< PoEAA / Repository
+@RepositoryRestResource(
+    collectionResourceRel = "inventories",
+    path = "inventories"
+)
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {}
---
+++ inventory/src/main/java/labshopmonolith/infra/PolicyHandler.java
@@ -0,0 +1,23 @@
+package labshopmonolith.infra;
+
+import java.util.function.Consumer;
+import labshopmonolith.domain.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+//<<< Clean Arch / Inbound Adaptor
+@Service
+@Transactional
+public class PolicyHandler {
+
+    @Bean
+    public Consumer<Message<?>> discardFunction() {
+        return message -> {
+            // Ingore unnecessary message
+            System.out.println("Discarded message: " + message);
+        };
+    }
+}
+//>>> Clean Arch / Inbound Adaptor
